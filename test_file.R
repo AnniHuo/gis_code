5c63e1fca7da037f86c5f4f11497520015286cf0
AH <38496860+AnniHuo@users.noreply.github.com>
2020-10-28 02:51:23 +0000

    Update test_file.R

--- test_file.R
+++ test_file.R
@@ -2,3 +2,13 @@
 print('the first line')
 
 print('Is it possible?')
+
+``` r
+A <- 1
+B <- 2
+C <- A+B
+C
+#> [1] 3
+```
+
+<sup>Created on 2020-10-28 by the [reprex package](https://reprex.tidyverse.org) (v0.3.0)</sup>
