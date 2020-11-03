df8d0f7aaf43e89c191626f70d45b255643c297a
AH <38496860+AnniHuo@users.noreply.github.com>
2020-11-03 13:24:58 +0000

    binder

--- test_file.R
+++ test_file.R
@@ -13,3 +13,10 @@ t <- 2
 # Added on github.com
 # test2
 
+library(remotes)
+remotes::install_github("karthik/holepunch")
+library(holepunch)
+write_compendium_description(package = "Your compendium name", 
+                             description = "Your compendium description")
+write_dockerfile(maintainer = "your_name") 
+generate_badge() 
