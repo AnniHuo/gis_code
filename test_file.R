79fc1c5c0c0e844e542089d0bb166fd8476a0a15
AH <38496860+AnniHuo@users.noreply.github.com>
2020-11-12 20:00:30 +0800

    new

--- test_file.R
+++ test_file.R
@@ -20,3 +20,5 @@ write_compendium_description(package = "Your compendium name",
                              description = "Your compendium description")
 write_dockerfile(maintainer = "your_name") 
 generate_badge() 
+
+build_binder()
